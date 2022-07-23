31ff25c191899c888bde26a5689cda2b16e95047
Jeonghyeon Park <dotsnangles@gmail.com>
2022-07-23 16:47:29 +0900

    20220722

--- app.py
+++ app.py
@@ -1,19 +1,28 @@
-from flask import Flask
+from crypt import methods
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__) # __name__ locates the app files
 
+questions = ['Is is compiled?', 'Does it run on a VM?']
 guesses = ['Python', 'Java', 'C++']
 
 @app.route('/') # Router
 def index():
-    return '<h1>Guess the Language!</h1>'
+    return render_template('index.html')
+
+# the default method of router is GET.
+@app.route('/question/<int:id>', methods=['GET', 'POST']) # if an argument variable is defined in the router, it's not going to be after query mark '?'.
+def question(id):
+    if request.method == 'POST':
+        if request.form['answer'] == 'yes':
+            return redirect(url_for('question', id=id+1))
+        else:
+            return redirect(url_for('question', id=id))
+    return render_template('question.html', question=questions[id])
 
 @app.route('/guess/<int:id>')
 def guess(id):
-    return (
-        '<h1>Guess the Language!</h1>'
-        f'<p>My guess: {guesses[id]}</p>'
-    )
+    return render_template('guess.html', guess=guesses[id])
     
             
 
