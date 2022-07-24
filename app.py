4c178735cdd7d1f81225f05eeb0df77e5f189d3c
Jeonghyeon Park <dotsnangles@gmail.com>
2022-07-24 11:35:34 +0900

    v0.9

--- app.py
+++ app.py
@@ -2,9 +2,14 @@ from crypt import methods
 from flask import Flask, render_template, request, redirect, url_for
 from flask_wtf import Form
 from wtforms.fields import RadioField, SubmitField
+from guess import Guess
 
 app = Flask(__name__) # __name__ locates the app files
 app.config['SECRET_KEY'] = 'secret!'
+
+game = Guess('Python')
+game.expand('Python', 'C++', 'Is it interpreted?', False)
+game.expand('C++', 'Java', 'Does it run on a VM?', True)
 
 questions = ['Is is compiled?', 'Does it run on a VM?']
 guesses = ['Python', 'Java', 'C++']
@@ -20,18 +25,20 @@ def index():
 # the default method of router is GET.
 @app.route('/question/<int:id>', methods=['GET', 'POST']) # if an argument variable is defined in the router, it's not going to be after query mark '?'.
 def question(id):
-    form = YesNoQuestionForm()
+    question = game.get_question(id)
+    if question == None:
+        return redirect(url_for('guess', id=id))
 
+    form = YesNoQuestionForm()
     if form.validate_on_submit():
-        if form.answer.data == 'yes':
-            return redirect(url_for('question', id=id+1))
-        else:
-            return redirect(url_for('question', id))
-    return render_template('question.html', question=questions[id], form=form)
+        new_id = game.answer_question(form.answer.data == 'yes', id)
+        return redirect(url_for('question', id=new_id))
+
+    return render_template('question.html', question=question, form=form)
 
 @app.route('/guess/<int:id>')
 def guess(id):
-    return render_template('guess.html', guess=guesses[id])
+    return render_template('guess.html', guess=game.get_guess(id))
     
             
 
