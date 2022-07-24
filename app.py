138f6a7d79011a898461c04bfcea9d58972b2091
Jeonghyeon Park <dotsnangles@gmail.com>
2022-07-24 10:50:48 +0900

    v0.8

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from crypt import methods
 from flask import Flask, render_template, request, redirect, url_for
+from flask_wtf import Form
+from wtforms.fields import RadioField, SubmitField
 
 app = Flask(__name__) # __name__ locates the app files
+app.config['SECRET_KEY'] = 'secret!'
 
 questions = ['Is is compiled?', 'Does it run on a VM?']
 guesses = ['Python', 'Java', 'C++']
+
+class YesNoQuestionForm(Form):
+    answer = RadioField('Your answer', choices=[('yes', 'Yes'), ('no', 'No')])
+    submit = SubmitField('Submit')
 
 @app.route('/') # Router
 def index():
@@ -13,12 +20,14 @@ def index():
 # the default method of router is GET.
 @app.route('/question/<int:id>', methods=['GET', 'POST']) # if an argument variable is defined in the router, it's not going to be after query mark '?'.
 def question(id):
-    if request.method == 'POST':
-        if request.form['answer'] == 'yes':
+    form = YesNoQuestionForm()
+
+    if form.validate_on_submit():
+        if form.answer.data == 'yes':
             return redirect(url_for('question', id=id+1))
         else:
-            return redirect(url_for('question', id=id))
-    return render_template('question.html', question=questions[id])
+            return redirect(url_for('question', id))
+    return render_template('question.html', question=questions[id], form=form)
 
 @app.route('/guess/<int:id>')
 def guess(id):
